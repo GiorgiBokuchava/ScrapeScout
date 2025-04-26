c895337dcfb149271f397eeb9d38c420898b7d24
GiorgiBokuchava <1giorgibokuchava@gmail.com>
2025-04-26 20:46:35 +0400

    Point Flask to ../templates and ../static

--- application/__init__.py
+++ application/__init__.py
@@ -9,8 +9,8 @@ import os
 # Correct static and template folders
 app = Flask(
     __name__,
-    static_folder="static",
-    template_folder="templates",
+    static_folder="../static",
+    template_folder="../templates",
     static_url_path="/static",
 )
 
