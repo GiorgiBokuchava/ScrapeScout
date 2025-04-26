4a34630d39a4b621c1bf827ce970d320087538a5
GiorgiBokuchava <1giorgibokuchava@gmail.com>
2025-04-26 20:25:11 +0400

    Move templates folder to root

--- application/__init__.py
+++ application/__init__.py
@@ -6,16 +6,19 @@ from flask_wtf import CSRFProtect
 from flask_migrate import Migrate
 import os
 
+# Get the base directory of the project
+base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
 # Correct static and template folders
 app = Flask(
     __name__,
-    static_folder="../static",
-    template_folder="../templates",
-    static_url_path="/static",
+    static_folder=os.path.join(base_dir, 'static'),
+    template_folder=os.path.join(base_dir, 'templates'),
+    static_url_path='/static',
 )
 
 # Configurations
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///instance/scrape.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{os.path.join(base_dir, 'instance', 'scrape.db')}"
 app.config["SECRET_KEY"] = os.getenv("SCRAPE_SCOUT_SECRET_KEY", "default_secret_key")
 
 # Extensions
